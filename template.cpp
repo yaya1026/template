25442e0332b637c21d473795eaa25092935134bb
YayaWatanabe <yaya.w.1026@gmail.com>
2017-09-03 11:54:37 +0900

    first commit

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#define REP(i, n) for (int i = 0; i < (n); i++)
+#define RREP(i, a, b) for (int i = a; i < (b); i++)
+#define FOR(i, n) for (int i = n; i >= 0; i--)
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <cstring>
+#include <vector>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    return 0;
+}
